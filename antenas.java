7d26037d034ee2556fddaa0b7938aa3618a6e525
BRAN <bpazp@ulasalle.edu.pe>
2026-06-07 21:49:05 -0500

    datos inciales antena

--- antenas.java
+++ antenas.java
@@ -1,3 +1,18 @@
 public class antenas {
-    
+    public static void main(String[] args) {
+        int[] casas = {1, 2, 7, 11, 20, 21, 30};
+        int cober = 5; 
+        
+        int n = casas.length; // n = 7
+        int cont = 0;
+        int i = 0;
+
+        
+        while (i < n) {
+            int dondePoner = casas[i] + cober; 
+            cont++;
+            System.out.println("Antena " + cont + " " + dondePoner);
+            
+        }
+    }
 }
